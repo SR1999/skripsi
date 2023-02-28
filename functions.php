02fa5232acdc2da2f109b66f78653037ee549d81
deceeeeee <micha3l_dct@yahoo.com>
2023-02-28 22:33:59 +0700

    Add try catch

--- functions.php
+++ functions.php
@@ -15,15 +15,22 @@ function query($query){
 function tambah($data){
 	global $conn;
 
+	/**
+	 * Collect Data
+	 */
 	$jabatan = $data["id_jabatan"];
 	$lokasi = $data["id_lokasi"];
 	$nama_karyawan = htmlspecialchars($data["nama_karyawan"]);
 	$nip = htmlspecialchars($data["nip"]);
 
-	$query = "INSERT INTO karyawan VALUES ('', $jabatan, $lokasi, '$nama_karyawan', '$nip')";
-	mysqli_query($conn, $query);
-
-	return mysqli_affected_rows($conn);
+	try {
+		$query = "INSERT INTO karyawan VALUES ('', $jabatan, $lokasi, '$nama_karyawan', '$nip')";
+		mysqli_query($conn, $query);
+		
+		return mysqli_affected_rows($conn);
+	} catch(Exception $e) {
+		print_r($e->getMessage());die;
+	}
 }
 
 
